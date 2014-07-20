3a1bb88017fc1cb75aafb6b6eb4e19cddc10281b
ThePromoter <pinciotti.dan182@gmail.com>
2014-07-20 19:07:28 -0400

    Code shown in class

---
+++ Matchismo/Matchismo/Model/CardMatchingGame.h
@@ -0,0 +1,22 @@
+//
+//  CardMatchingGame.h
+//  Matchismo
+//
+//  Created by Dan Pinciotti on 7/20/14.
+//  Copyright (c) 2014 Dan Pinciotti. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Deck.h"
+
+@interface CardMatchingGame : NSObject
+
+// Designated initializer
+- (instancetype)initWithCardCount:(NSUInteger)count usingDeck:(Deck *)deck;
+
+- (void)chooseCardAtIndex:(NSUInteger)index;
+- (Card *)cardAtIndex:(NSUInteger)index;
+
+@property (nonatomic, readonly) NSInteger score;
+
+@end
